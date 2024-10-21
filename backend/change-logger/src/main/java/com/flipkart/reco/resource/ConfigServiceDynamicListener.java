7902332d2a4d4bd4765fb8d959420f251845a25f
man-fk <gutta.manoj@flipkart.com>
2024-10-21 20:58:38 +0530

    working for env variables

--- backend/change-logger/src/main/java/com/flipkart/reco/resource/ConfigServiceDynamicListener.java
+++ backend/change-logger/src/main/java/com/flipkart/reco/resource/ConfigServiceDynamicListener.java
@@ -4,7 +4,6 @@ import com.flipkart.kloud.config.BaseBucketUpdateListener;
 import com.flipkart.kloud.config.Bucket;
 import com.flipkart.kloud.config.DynamicBucket;
 import com.flipkart.kloud.config.error.ConfigServiceException;
-import com.flipkart.reco.service.ConfigService;
 import com.flipkart.reco.service.ConfigUpdateDao;
 import com.flipkart.reco.repository.ConfigUpdateRepository;
 import com.flipkart.reco.util.ConfigListenerUtil;
@@ -17,17 +16,15 @@ public class ConfigServiceDynamicListener {
     public ConfigServiceDynamicListener(String configBucketName, String zone, ConfigUpdateRepository configUpdateRepository) {
         ConfigUpdateDao configUpdateDao = new ConfigUpdateDao(configUpdateRepository);
         try {
-            DynamicBucket bucket = ConfigListenerUtil.getInstance().getConfigServiceDynamicBucket(configBucketName);
-            String url = "http://"+ ConfigListenerUtil.getInstance().getEndpoints().get("asia-south1").get("default").host + "/v1/history/";
-            ConfigService configService = new ConfigService();
-            configService.makeDBConsistent(url, configBucketName, zone, configUpdateDao);
+            DynamicBucket bucket = ConfigListenerUtil.getInstance1().getConfigServiceDynamicBucket(configBucketName);
+            ConfigListenerUtil.getInstance1().makeDBConsistent(configBucketName, zone, configUpdateDao);
             if(bucket != null) {
             bucket.addListener(new BaseBucketUpdateListener() {
                 @SneakyThrows
                 @Override
                 public void updated(Bucket oldBucket, Bucket newBucket) {
-                    int version = configService.getLatestVersion(url,configBucketName);
-                    String json = configService.getUpdateByVersion(url,configBucketName,version);
+                    int version = ConfigListenerUtil.getInstance1().getLatestVersion(configBucketName);
+                    String json = ConfigListenerUtil.getInstance1().getUpdateByVersion(configBucketName,version);
                     try {
                         configUpdateDao.save(configBucketName, zone, json);
                     } catch (ParseException e) {
--- backend/change-logger/src/main/java/com/flipkart/reco/util/ConfigListenerUtil.java
+++ backend/change-logger/src/main/java/com/flipkart/reco/util/ConfigListenerUtil.java
@@ -2,10 +2,20 @@ package com.flipkart.reco.util;
 
 import com.flipkart.kloud.config.*;
 import com.flipkart.kloud.config.error.ConfigServiceException;
+import com.flipkart.reco.service.ConfigUpdateDao;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,18 +23,19 @@ import java.util.concurrent.Executors;
 public class ConfigListenerUtil {
     private final ExecutorService executorService;
     private final DynamicBucketCache dynamicBucketCache;
+    private final Client client;
+    private final String url;
     private final HttpClient httpClient;
     @Getter
-    static ConfigListenerUtil instance = new ConfigListenerUtil();
-    @Getter
-    Map<String, Map<String, ConfigServiceEndpoint>> endpoints;
+    static ConfigListenerUtil instance1 = new ConfigListenerUtil();
     private ConfigListenerUtil() {
+        client = ClientBuilder.newClient();
         executorService = Executors.newFixedThreadPool(20,
                 r -> new Thread(r, "ConfigWatchThread"));
         dynamicBucketCache = new DynamicBucketCache();
         InstanceMetaData instanceMetaData = InstanceMetaData.singleton();
-        endpoints = EndpointProvider.ZONE_VPC_TO_ENDPOINT_MAP;
-        ConfigServiceEndpoint configServiceEndpoint = endpoints.get("asia-south1").get("default");
+        ConfigServiceEndpoint configServiceEndpoint = EndpointProvider.singleton().endpoint();
+        url =  "http://"+ configServiceEndpoint.host + "/v1/history/";
         httpClient = new HttpClientBuilder().host(configServiceEndpoint.host)
                 .port(configServiceEndpoint.port).apiVersion(configServiceEndpoint.apiVersion)
                 .connectTimeout(5000).executorService(executorService).connections(20)
@@ -42,5 +53,43 @@ public class ConfigListenerUtil {
             log.error("Error fetching bucket {}", bucketName, e);
         }
         return dynamicBucket;
+    }
+
+    public void makeDBConsistent(String name, String zone, ConfigUpdateDao configUpdateDao) throws ParseException {
+        int version = getLatestVersion(name);
+        int db_version;
+        Integer v = configUpdateDao.findHighestVersionByName(name);
+        db_version = Objects.requireNonNullElse(v, 0);
+        db_version++;
+        while(db_version != version+1)
+        {
+            String json1 = getUpdateByVersion(name,db_version);
+            try {
+                configUpdateDao.save(name,zone,json1);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            db_version++;
+        }
+    }
+
+    public int getLatestVersion(String configBucketName) throws ParseException {
+        String json = getLatestUpdate(configBucketName);
+        JSONParser parser = new JSONParser();
+        JSONArray arr = (JSONArray) parser.parse(json);
+        JSONObject obj = (JSONObject) arr.get(0);
+        return ((Long) obj.get("version")).intValue();
+    }
+    public String getLatestUpdate(String configBucketName){
+        WebTarget target = client.target(url).queryParam("limit", "1").path(configBucketName);
+        Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        return response.readEntity(String.class);
+    }
+    public String getUpdateByVersion(String configBucketName,int version) {
+        WebTarget target = client.target(url).path(configBucketName).path(String.valueOf(version));
+        Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        return response.readEntity(String.class);
     }
 }
